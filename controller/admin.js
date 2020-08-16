cb351037c17c61b88dd8f144552f5666d545f76d
Your Name <you@example.com>
2020-08-16 23:09:40 +0600

    admin login

--- controller/admin.js
+++ controller/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var userModel = require.main.require('./models/user-model');
+var userModel = require.main.require('./models/register');
 var db 		= require.main.require('./models/db');
 var router = express.Router();
 
@@ -20,5 +21,30 @@ router.get('/', function(req, res){
 });
 
 
+router.get('/', function(req, res){
+	res.render('register/index');
+});
+
+router.post('/', function(req, res){
+
+	var user = {
+		name    : req.body.name,
+		username: req.body.username,
+		password: req.body.password,
+	  contactno	: req.body.contactno,
+		user : req.body.user
+	}
+
+	userModel.register(user, function(status){
+
+		if(status){
+
+			res.redirect('/login');
+		}else{
+			res.send('invalid username/password');
+		}
+	});
+
+});
 
 module.exports = router;
